59edf6a58b090a13f3d487eb9c952f3df2939601
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 14:53:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   def apply_discount
     puts @total
     puts @discount
-    @total = @total * (1 - (@discount / 100)) if @discount > 0
+    @total *= (1 - (@discount / 100)) if @discount > 0
     puts @total
     @total
   end
