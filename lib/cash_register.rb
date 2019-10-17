5807d994915e59cd818a54967f0f74b7dd41b1d7
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 14:57:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
       return "There is no discount to apply."
     else
       @total *= (1 - (@discount.to_f / 100.0)) if @discount > 0
-      return "After the discount, the total comes to $#{@total.to_i}."
+      "After the discount, the total comes to $#{@total.to_i}."
     end
   end
 
