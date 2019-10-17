38baab0cb37c1364fd01acd7966c37ad31724ade
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 15:00:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @total += price * quantity
     @last_transaction = { item => price }
+    @items << item
   end
 
   def apply_discount
