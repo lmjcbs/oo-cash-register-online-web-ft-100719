9ae7e27089d3e235cb77c336b004986164f192f7
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 14:46:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :cash_register_total, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity = 1)
+
+    @total += price * quantity
+    @last_transaction = { item => price }
+  end
+
+  def apply_discount
+    @total *= (@discount / 100).to_f if @discount > 0
+    @total
+  end
+
+end
